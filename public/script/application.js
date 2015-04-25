4bed7789697df286d386263b5661462c56bebac4
Marcus Noble <marcus@marcusnoble.co.uk>
2015-04-25 12:21:46 +0100

    Added drag+drop

--- public/script/application.js
+++ public/script/application.js
@@ -1,8 +1,21 @@
 (function(){
 	var init = function init(){
 		$('.issue-col').height($('.issue-col').height());
+		$('.issue-col').each(function(){
+			this.addEventListener('drop', onDrop);
+		});
+		$('.issue').each(function(){
+			this.addEventListener('dragstart', startDrag);
+		});		
 	};
 
+	var startDrag = function startDrag(e){
+		e.dataTransfer.setData('text/plain', this.id);
+	};
+	var onDrop = function onDrop(e){
+		var issue = $('#'+event.dataTransfer.getData('text/plain'));
+		issue.remove().appendTo($(e.target).closest('.issue-col'));
+	};
 
 	init();
 }());
