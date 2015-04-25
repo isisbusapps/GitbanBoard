5f0e6249380712094b29b657987fbe2aae357c6b
Marcus Noble <marcus@marcusnoble.co.uk>
2015-04-25 13:55:40 +0100

    Moved repo list to environments

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,12 @@
 var express = require('express'),
 	router = express.Router(),
 	github = require('../helpers/GitHub');
+require('dotenv').load();
 
 router.get('/', function(req, res) {
 	github.getIssues(
 	{
-		repos: ['jsoxford.github.com']
+		repos: process.env.REPOS.split(',')
 	},
 	function(issues){
   		res.render('kanban', {backlog:issues,todo:[],inprogress:[],done:[]});
