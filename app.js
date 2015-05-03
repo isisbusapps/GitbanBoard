bd26f139fc288827174577336f407da99df71fb4
Marcus Noble <marcus.noble@stfc.ac.uk>
2015-05-03 18:59:35 +0100

    Small tweaks

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(require('./controllers/auth'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('Not Found (' + req.url + ')');
     err.status = 404;
     next(err);
 });
