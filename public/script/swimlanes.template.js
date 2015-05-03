405f120ffd3289ec34d6ef1f418d1cc95d48b45a
Marcus Noble <marcus.noble@stfc.ac.uk>
2015-05-03 22:36:37 +0100

    Fixed drag+drop

--- public/script/swimlanes.template.js
+++ public/script/swimlanes.template.js
@@ -7,6 +7,6 @@ templates['swimlane'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":functi
     + alias3(((helper = (helper = helpers.assignee || (depth0 != null ? depth0.assignee : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"assignee","hash":{},"data":data}) : helper)))
     + "\">\n    <div class=\"swimlane-label\">"
     + alias3(((helper = (helper = helpers.assignee || (depth0 != null ? depth0.assignee : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"assignee","hash":{},"data":data}) : helper)))
-    + "</div>\n    <div data-column=\"backlog-col\" class=\"col-sm-3 issue-col\">\n    </div>\n    <div data-column=\"todo-col\" class=\"col-sm-3 issue-col\">\n    </div>\n    <div data-column=\"inprogress-col\" class=\"col-sm-3 issue-col\">\n    </div>\n    <div data-column=\"done-col\" class=\"col-sm-3 issue-col\">\n    </div>\n</div>";
+    + "</div>\n    <div data-column=\"backlog-col\" class=\"col-sm-3 issue-col\" ondragover=\"return false;\">\n    </div>\n    <div data-column=\"todo-col\" class=\"col-sm-3 issue-col\" ondragover=\"return false;\">\n    </div>\n    <div data-column=\"inprogress-col\" class=\"col-sm-3 issue-col\" ondragover=\"return false;\">\n    </div>\n    <div data-column=\"done-col\" class=\"col-sm-3 issue-col\" ondragover=\"return false;\">\n    </div>\n</div>";
 },"useData":true});
 })();
